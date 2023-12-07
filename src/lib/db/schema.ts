abbf6f7738d99350af7f5b3908fb52536a876f14
Abdulrahman Mohamad Aarabi <abma@arteliagroup.dk>
2023-12-07 12:27:40 +0100

    fixing bug

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,31 @@
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+  pgEnum,
+} from "drizzle-orm/pg-core";
+
+export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"]);
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf-name").notNull(),
+  pdfUrl: text("pdf-url").notNull(),
+  createdAt: timestamp("created-at").notNull().defaultNow(),
+  userId: varchar("user-id", { length: 256 }).notNull(),
+  fileKey: text("file-key").notNull(),
+});
+
+export type DrizzleChat = typeof chats.$inferSelect;
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: userSystemEnum("role").notNull(),
+});
