b4a6bd03fe4b32332063a5a8a2fc60e411495a59
Abdulrahman Mohamad Aarabi <abma@arteliagroup.dk>
2023-12-07 12:40:15 +0100

    add fun

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function convertToAscii(inputString: string): string {
+  // Replace non-ASCII characters and spaces with underscores
+  const asciiString = inputString
+    .replace(/[^\x20-\x7E]+/g, "")
+    .replace(/\s+/g, "_");
+  console.log(asciiString);
+
+  return asciiString;
 }
