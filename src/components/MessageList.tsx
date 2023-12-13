a647871d2b3a0c94a389a4e40b761036acac3b85
Abdulrahman Mohamad Aarabi <abma@arteliagroup.dk>
2023-12-13 13:27:56 +0100

    disable eslint props

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -95,39 +95,12 @@ const MessageList = ({ messages, isLoading, msgIsloading }: Props) => {
               >
                 {message.content}
               </Markdown> */}
+
               <Markdown
                 className="pl-8 font-mono mb-4 max-sm:text-base"
                 rehypePlugins={[rehypeRaw, rehypeKatex]}
                 remarkPlugins={[remarkGfm, remarkMath]}
-                components={{
-                  code(props) {
-                    const { children, className, node, ...rest } = props;
-                    const match = /language-(\w+)/.exec(className || "");
-                    return match ? (
-                      //@ts-ignore
-                      <SyntaxHighlighter
-                        {...rest}
-                        PreTag="div"
-                        children={String(children).replace(/\n$/, "")}
-                        language={match[1]}
-                        style={a11yDark}
-                        showLineNumbers
-                      />
-                    ) : (
-                      <code {...rest} className={className}>
-                        {children}
-                      </code>
-                    );
-                  },
-                }}
-              >
-                {message.content}
-              </Markdown>
-
-              {/* <Markdown
-                className="pl-8 font-mono mb-4 max-sm:text-base"
-                rehypePlugins={[rehypeRaw, rehypeKatex]}
-                remarkPlugins={[remarkGfm, remarkMath]}
+                // eslint-disable-next-line react/no-children-prop
                 children={message.content}
                 components={{
                   code(props) {
@@ -150,7 +123,7 @@ const MessageList = ({ messages, isLoading, msgIsloading }: Props) => {
                     );
                   },
                 }}
-              /> */}
+              />
             </div>
           </div>
         );
